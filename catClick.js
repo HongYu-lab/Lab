0c2a8bb91154d6895338f195972d5c39aad2087f
HongYu-lab <jordan87150699@gmail.com>
2024-08-08 14:32:28 +0800

    新增提示

--- catClick.js
+++ catClick.js
@@ -2,8 +2,8 @@ function catClick(svg) {
     svg.addEventListener("load", function () {
         const catfound = svg.contentDocument.querySelectorAll('[class^=catNum]');//used by clickEvent
         const catsSVG = svg.contentDocument.getElementById("catsSVG"); //used by drag and zoom
-        let moving; //used bu drag
         let countdown = 100;
+        document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
 
         //設定全部未點擊 clicked = 0
         for (let i = 0; i < catfound.length; i++) {
@@ -20,6 +20,8 @@ function catClick(svg) {
                     catfound[i].setAttribute("fill", color);
                     catfound[i].setAttribute("clicked", 1);
                     countdown -= 1;
+                    document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
+
                     // console.log(countdown);
                 }
                 else if ((hasTailClass(catfound[i]) == 1) && (catfound[i].getAttribute("clicked") != 1)) {
@@ -29,17 +31,21 @@ function catClick(svg) {
                     catfound[i].setAttribute("fill", color);
                     catfound[i].setAttribute("clicked", 1);
                     countdown -= 1;
+                    document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
+
                     // console.log(countdown);
                 }
                 else if (catfound[i].getAttribute("clicked") != 1) {
                     catfound[i].setAttribute("fill", randomColor());
                     catfound[i].setAttribute("clicked", 1);
                     countdown -= 1;
+                    document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
+
                     // console.log(countdown);
                 }
                 else
                     console.log("點過了別再點了QQ");
-            })
+            });
         }
         //計時器
         let i = 0;
@@ -51,112 +57,61 @@ function catClick(svg) {
                         timevoer = 1;
                     else {
                         document.getElementById("timer").innerHTML = `計時已過: ${i}秒`;
-                        document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
-                        // console.log(countdown);
+                        // console.log(countdown);  
+                        if (i == 2)
+                            document.getElementById("hint").style.visibility = "visible";
                     }
                 }, 1000 * i)
             })(i++)
-
             if (timevoer == 1)
                 break;
         }
-        //drag 拖曳
-        catsSVG.addEventListener("mousedown", function (e) {
-            moving = true;
-        });
-        catsSVG.addEventListener("mousemove", function (e) {
-            if (moving === true) {
-                // 1. 取得一開始的 viewBox 字串轉數字
-                let startViewBox = catsSVG
-                    .getAttribute("viewBox")
-                    .split(" ")
-                    .map((n) => parseFloat(n));
-
-                //  2. 取得滑鼠當前 viewport 中 滑鼠座標
-                let startClient = {
-                    x: e.clientX,
-                    y: e.clientY,
-                };
-                //  3. 計算對應回去的 SVG 座標值
-                let newSVGPoint = catsSVG.createSVGPoint();
-                let CTM = catsSVG.getScreenCTM();
-                newSVGPoint.x = startClient.x;
-                newSVGPoint.y = startClient.y;
-                let startSVGPoint = newSVGPoint.matrixTransform(CTM.inverse());
-                //  4. 計算拖曳後滑鼠所在的 viewport 滑鼠座標
-                let moveToClient = {
-                    x: e.clientX + e.movementX,
-                    y: e.clientY + e.movementY,
-                };
-                //  5. 計算對應回去的 SVG 座標值
-                newSVGPoint = catsSVG.createSVGPoint();
-                CTM = catsSVG.getScreenCTM();
-                newSVGPoint.x = moveToClient.x;
-                newSVGPoint.y = moveToClient.y;
-                let moveToSVGPoint = newSVGPoint.matrixTransform(CTM.inverse());
-                //  6. 計算位移量
-                let delta = {
-                    dx: startSVGPoint.x - moveToSVGPoint.x,
-                    dy: startSVGPoint.y - moveToSVGPoint.y,
-                };
-                //  7. 設定新的 viewBox 值
-                if ((startViewBox[0] + delta.dx) < 0)
-                    startViewBox[0] = 0;
-                else if (startViewBox[0] + startViewBox[2] > 1440)
-                    startViewBox[0] = 1440 - startViewBox[2];
-                else
-                    startViewBox[0] += delta.dx;
-                if ((startViewBox[1] + delta.dy) < 0)
-                    startViewBox[1] = 0;
-                else
-                    startViewBox[1] += delta.dy;
-
-                let moveToViewBox = `${startViewBox[0]} ${startViewBox[1]} ${startViewBox[2]
-                    } ${startViewBox[3]}`;
-                catsSVG.setAttribute('viewBox', moveToViewBox);
-                // console.log(moveToViewBox);
-            }
-        });
-        catsSVG.addEventListener("mouseup", function (e) {
-            moving = false;
-        });
-        //zoom 放大縮小
-        catsSVG.addEventListener("wheel", function (e) {
-            //  1.取得一開始的 viewBox。
-            let startViewBox = catsSVG
-                .getAttribute("viewBox")
-                .split(" ")
-                .map((n) => parseFloat(n));
-            // 2.1 取得滑鼠座標
-            let startClient = {
-                x: e.clientX,
-                y: e.clientY,
-            };
-            //  3. 計算對應回去的 SVG 座標值
-            let newSVGPoint = catsSVG.createSVGPoint();
-            let CTM = catsSVG.getScreenCTM();
-            newSVGPoint.x = startClient.x;
-            newSVGPoint.y = startClient.y;
-            //縮放 r 倍
-            let r;
-            if (e.deltaY > 0) {
-                r = 1.05;
-            } else if (e.deltaY < 0) {
-                r = 0.95;
-            } else {
-                r = 1;
-            }
-            if ((startViewBox[2] * r) < 1450) {
-                startViewBox[2] *= r;
-                startViewBox[3] *= r;
-            }
-            let zoomToViewBox = `${startViewBox[0]} ${startViewBox[1]} ${startViewBox[2]
-                } ${startViewBox[3]}`;
-            // console.log(zoomToViewBox);
-            catsSVG.setAttribute("viewBox", zoomToViewBox);
-        })
+        drag(catsSVG);
+        zoom(catsSVG);
+        hint(catfound);
     });
 }
+
+function hint(catfound) {
+    let hint = document.getElementById("hint");
+    let check = [];
+    hint.addEventListener("click", function () {
+        for (let i = 0; i < catfound.length; i++) {
+            if (catfound[i].getAttribute("clicked") == 0)
+                check.push(catfound[i]);
+        }
+        let a = Math.floor((Math.random() * 100) % check.length);
+        console.log(getFirstClass(check[a]));
+        // check[a].setAttribute("fill", color);
+        flash(check[a]);
+        while(check.length)
+        {
+            check.pop();
+        }
+    });
+}
+function flash(check) {
+    let color = "#47473F"
+    let flag = 0;
+    let i = 0;
+    while (i < 6) {
+        (function (i) {
+
+            setTimeout(function () {
+                if (flag == 0) {
+                    check.setAttribute("fill", color);
+                    flag = 1;
+                }
+                else {
+                    check.setAttribute("fill", "#ffffff");
+                    flag = 0;
+                }
+                // console.log(countdown);  
+            }, 125 * i)
+        })(i++)
+    }
+}
+
 function getFirstClass(elem) {
     let str = elem.getAttribute("class").split(" ")[0];
     return str;
@@ -194,17 +149,17 @@ function randomColor() {
     //  #A3C3DA 81~90
     //  #FFCBCB 91~100
 
-    let a = Math.floor((Math.random() * 10) % 10);
+    let a = Math.floor((Math.random() * 100) % 10);
     let color;
     switch (a) {
         case 1: color = "#FFEBA2"; break;
-        case 2: color = "#31AE3D"; break;
+        case 2: color = "#66DB72"; break;
         case 3: color = "#BAAFFF"; break;
-        case 4: color = "#49EEF9"; break;
+        case 4: color = "#90EBF1"; break;
         case 5: color = "#F8B5F1"; break;
         case 6: color = "#8CFFC8"; break;
         case 7: color = "#BAC9FF"; break;
-        case 8: color = "#FFA14A"; break;
+        case 8: color = "#F3BC8A"; break;
         case 9: color = "#A3C3DA"; break;
         case 0: color = "#FFCBCB"; break;
         default: break;
@@ -212,5 +167,102 @@ function randomColor() {
     return color;
 }
 
+function zoom(catsSVG) {
+    //zoom 放大縮小
+    catsSVG.addEventListener("wheel", function (e) {
+        //  1.取得一開始的 viewBox。
+        let startViewBox = catsSVG
+            .getAttribute("viewBox")
+            .split(" ")
+            .map((n) => parseFloat(n));
+        // 2.1 取得滑鼠座標
+        let startClient = {
+            x: e.clientX,
+            y: e.clientY,
+        };
+        //  3. 計算對應回去的 SVG 座標值
+        let newSVGPoint = catsSVG.createSVGPoint();
+        newSVGPoint.x = startClient.x;
+        newSVGPoint.y = startClient.y;
+        //縮放 r 倍
+        let r;
+        if (e.deltaY > 0) {
+            r = 1.05;
+        } else if (e.deltaY < 0) {
+            r = 0.95;
+        } else {
+            r = 1;
+        }
+        if ((startViewBox[2] * r) < 1450) {
+            startViewBox[2] *= r;
+            startViewBox[3] *= r;
+        }
+        let zoomToViewBox = `${startViewBox[0]} ${startViewBox[1]} ${startViewBox[2]
+            } ${startViewBox[3]}`;
+        // console.log(zoomToViewBox);
+        catsSVG.setAttribute("viewBox", zoomToViewBox);
+    });
+}
+function drag(catsSVG) {
+    //drag 拖曳
+    let moving;
+    catsSVG.addEventListener("mousedown", function (e) {
+        moving = true;
+    });
+    catsSVG.addEventListener("mousemove", function (e) {
+        if (moving === true) {
+            // 1. 取得一開始的 viewBox 字串轉數字
+            let startViewBox = catsSVG
+                .getAttribute("viewBox")
+                .split(" ")
+                .map((n) => parseFloat(n));
 
+            //  2. 取得滑鼠當前 viewport 中 滑鼠座標
+            let startClient = {
+                x: e.clientX,
+                y: e.clientY,
+            };
+            //  3. 計算對應回去的 SVG 座標值
+            let newSVGPoint = catsSVG.createSVGPoint();
+            let CTM = catsSVG.getScreenCTM();
+            newSVGPoint.x = startClient.x;
+            newSVGPoint.y = startClient.y;
+            let startSVGPoint = newSVGPoint.matrixTransform(CTM.inverse());
+            //  4. 計算拖曳後滑鼠所在的 viewport 滑鼠座標
+            let moveToClient = {
+                x: e.clientX + e.movementX,
+                y: e.clientY + e.movementY,
+            };
+            //  5. 計算對應回去的 SVG 座標值
+            newSVGPoint = catsSVG.createSVGPoint();
+            CTM = catsSVG.getScreenCTM();
+            newSVGPoint.x = moveToClient.x;
+            newSVGPoint.y = moveToClient.y;
+            let moveToSVGPoint = newSVGPoint.matrixTransform(CTM.inverse());
+            //  6. 計算位移量
+            let delta = {
+                dx: startSVGPoint.x - moveToSVGPoint.x,
+                dy: startSVGPoint.y - moveToSVGPoint.y,
+            };
+            //  7. 設定新的 viewBox 值
+            if ((startViewBox[0] + delta.dx) < 0)
+                startViewBox[0] = 0;
+            else if (startViewBox[0] + startViewBox[2] > 1440)
+                startViewBox[0] = 1440 - startViewBox[2];
+            else
+                startViewBox[0] += delta.dx;
+            if ((startViewBox[1] + delta.dy) < 0)
+                startViewBox[1] = 0;
+            else
+                startViewBox[1] += delta.dy;
 
+            let moveToViewBox = `${startViewBox[0]} ${startViewBox[1]} ${startViewBox[2]
+                } ${startViewBox[3]}`;
+            catsSVG.setAttribute('viewBox', moveToViewBox);
+            // console.log(moveToViewBox);
+        }
+    });
+    catsSVG.addEventListener("mouseup", function (e) {
+        moving = false;
+    });
+}
