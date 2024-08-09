9734dc83a3e25300a3b591a34f1235d9c51e8122
HongYu-lab <jordan87150699@gmail.com>
2024-08-09 11:47:38 +0800

    新增圖片、背景

--- catClick.js
+++ catClick.js
@@ -3,8 +3,6 @@ function catClick(svg) {
         const catfound = svg.contentDocument.querySelectorAll('[class^=catNum]');//used by clickEvent
         const catsSVG = svg.contentDocument.getElementById("catsSVG"); //used by drag and zoom
         let countdown = 100;
-        document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
-
         //設定全部未點擊 clicked = 0
         for (let i = 0; i < catfound.length; i++) {
             catfound[i].setAttribute("clicked", "0");
@@ -20,7 +18,7 @@ function catClick(svg) {
                     catfound[i].setAttribute("fill", color);
                     catfound[i].setAttribute("clicked", 1);
                     countdown -= 1;
-                    document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
+                    printCountdown(countdown);
                     if (countdown == 0)
                         alert("AAAA");
                     // console.log(countdown);
@@ -32,17 +30,16 @@ function catClick(svg) {
                     catfound[i].setAttribute("fill", color);
                     catfound[i].setAttribute("clicked", 1);
                     countdown -= 1;
-                    document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
+                    printCountdown(countdown);
                     if (countdown == 0)
                         alert("AAAA");
-
                     // console.log(countdown);
                 }
                 else if (catfound[i].getAttribute("clicked") != 1) {
                     catfound[i].setAttribute("fill", randomColor());
                     catfound[i].setAttribute("clicked", 1);
                     countdown -= 1;
-                    document.getElementById("catRemain").innerHTML = `還剩下: ${countdown}隻`;
+                    printCountdown(countdown);
                     if (countdown == 0)
                         alert("AAAA");
 
@@ -59,16 +56,16 @@ function catClick(svg) {
             (function (i) {
                 setTimeout(function () {
                     if (countdown == 0) {
+                        //作弊碼
                         // for (let i = 0; i < catfound.length; i++) {
                         //     catfound[i].setAttribute("fill", "#DC803C");
                         // }
                         timeover = 1;
                     }
-                    else {
-                        document.getElementById("timer").innerHTML = `計時已過: ${i}秒`;
-                        // console.log(countdown);  
+                    else {                     
+                        printTimer(i);
                         if (i == 2)
-                            document.getElementById("hint").style.visibility = "visible";
+                            document.getElementById("hintCombine").style.visibility = "visible";
                     }
                 }, 1000 * i)
             })(i++)
@@ -79,6 +76,48 @@ function catClick(svg) {
         zoom(catsSVG);
         hint(catfound);
     });
+}
+function printCountdown(countdown) {
+    let firsrtDigit = Math.floor(countdown / 100);
+    let secondDigit = Math.floor((countdown % 100) / 10);
+    let thirdDigit = Math.floor(countdown % 10);
+
+    let firsrtDigitImg = numImages(firsrtDigit);
+    let secondDigitImg = numImages(secondDigit);
+    let thirdDigitImg = numImages(thirdDigit);
+    document.getElementById("num1").setAttribute("src", firsrtDigitImg);
+    document.getElementById("num2").setAttribute("src", secondDigitImg);
+    document.getElementById("num3").setAttribute("src", thirdDigitImg);
+}
+function printTimer(time) {
+    let firsrtDigit = Math.floor(time / 100);
+    let secondDigit = Math.floor((time % 100) / 10);
+    let thirdDigit = Math.floor(time % 10);
+
+    let firsrtDigitImg = numImages(firsrtDigit);
+    let secondDigitImg = numImages(secondDigit);
+    let thirdDigitImg = numImages(thirdDigit);
+    document.getElementById("timer1").setAttribute("src", firsrtDigitImg);
+    document.getElementById("timer2").setAttribute("src", secondDigitImg);
+    document.getElementById("timer3").setAttribute("src", thirdDigitImg);
+}
+
+function numImages(num) {
+    let srcString = "";
+    switch (num) {
+        case 0: srcString = "./images/num0.svg"; break;
+        case 9: srcString = "./images/num9.svg"; break;
+        case 8: srcString = "./images/num8.svg"; break;
+        case 7: srcString = "./images/num7.svg"; break;
+        case 6: srcString = "./images/num6.svg"; break;
+        case 5: srcString = "./images/num5.svg"; break;
+        case 4: srcString = "./images/num4.svg"; break;
+        case 3: srcString = "./images/num3.svg"; break;
+        case 2: srcString = "./images/num2.svg"; break;
+        case 1: srcString = "./images/num1.svg"; break;
+        default: break;
+    }
+    return srcString;
 }
 
 function hint(catfound) {
@@ -256,6 +295,7 @@ function drag(catsSVG) {
             if ((startViewBox[0] + delta.dx) < 0)
                 startViewBox[0] = 0;
             else if (startViewBox[0] + startViewBox[2] > 1440)
+                // console.log("do nothing");
                 startViewBox[0] = 1440 - startViewBox[2];
             else
                 startViewBox[0] += delta.dx;
